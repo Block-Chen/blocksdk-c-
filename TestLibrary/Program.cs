a3f0023a5e6e590221741fdb9528056b791eb5a4
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2020-10-15 10:54:19 +0900

    Add files via upload

---
+++ TestLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    class Program {
+        static async Task Main(string[] args)
+        {
+
+            // var bas = new Base("aaa");
+            // bas.request("GET", "", new String[] { "" });
+
+
+			blockSDK = new BlockSDK("YOUR TOKEN");
+
+
+
+			ltcClient = blockSDK.createLitecoin("YOUR TOKEN")
+			output = ltcClient.getBlockChain("YOUR TOKEN")
+
+			print(output)
+
+
+
+        }
+    }
+}
